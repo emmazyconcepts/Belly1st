e43ce7f6abad3a64acbd0275af4f6ceea93bfb67
ekeemmanuel <emmazy100@gmail.com>
2023-01-24 02:12:06 +0100

    header

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,7 +1,7 @@
 import { View, Text, SafeAreaView, Image } from "react-native";
 import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
-import { UserCircleIcon } from "react-native-heroicons/outline";
+import { UserCircleIcon, WalletIcon } from "react-native-heroicons/outline";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
@@ -13,19 +13,28 @@ const HomeScreen = () => {
   }, []);
 
   return (
-    <SafeAreaView>
-      <Text>
-        <View>
-          <UserCircleIcon size={40} color="#EA3C12" />
+    <SafeAreaView className="bg-white">
+      <View className=" flex-row pb-3 items-center mx-4 space-x-2">
+        <UserCircleIcon size={40} color="#EA3C12" />
 
-          {/* <Image
+        {/* <Image
             className=" h-10 w-10 bg-gray-400 rounded-full"
             source={{
               url: "https://img.icons8.com/external-gradak-royyan-wijaya/24/null/external-circle-gradak-user-gradak-royyan-wijaya-3.png",
             }}
           /> */}
+        <View className=" flex-1">
+          <Text className=" font-bold text-gray-400 text-xs">Good Morning</Text>
+          <Text className=" font-bold text-xl ">Eke Emmanuel</Text>
         </View>
-      </Text>
+        <View>
+          <Text className=" font-bold text-gray-400 text-xs">
+            {" "}
+            <WalletIcon color="#EA3C12" /> Wallet Balance
+          </Text>
+          <Text>#12,000</Text>
+        </View>
+      </View>
     </SafeAreaView>
   );
 };
