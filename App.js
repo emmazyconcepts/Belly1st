9467e5a05a89fc8556c73129687472d04e1115da
ekeemmanuel <emmazy100@gmail.com>
2023-01-24 01:29:47 +0100

    add

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View className=" justify-center items-center flex-1">
+        <Text>Belly1st</Text>
+        <Text className="text-pink-500 ">Foods made easy for you and me </Text>
+
+        <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
